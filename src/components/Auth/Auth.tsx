4305b2359464ecefef4a7906770567bdaf0517d5
martin-g-petrucci <martin.petrucci@uala.com.ar>
2024-02-26 20:14:50 -0300

    sha256

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { BASE_URL } from "@/utils/meli";
+import { sha256 } from "js-sha256";
 import { usePathname, useParams, useSearchParams } from "next/navigation";
 
 export default function Auth() {
@@ -18,15 +19,16 @@ export default function Auth() {
     console.log({ data });
   }
 
+  const hash = sha256("12345");
+  const url = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=7523285884318227&redirect_uri=https://search-car-xi.vercel.app/&code_challenge=${hash}&code_challenge_method=S256`;
   return (
     <div>
       <p>code: {code}</p>
       {code ? (
         <span>is authenticated</span>
       ) : (
-        <a href="https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=7523285884318227&redirect_uri=https://search-car-xi.vercel.app/&code_challenge=12345&code_challenge_method=plain">
-          auth
-        </a>
+        // <a href="https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=7523285884318227&redirect_uri=https://search-car-xi.vercel.app/&code_challenge=12345&code_challenge_method=plain">
+        <a href={url}>auth</a>
       )}
     </div>
   );
