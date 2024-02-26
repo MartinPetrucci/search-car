b792066984c20c9344619e5f65768b11ea089f0f
martin-g-petrucci <martin.petrucci@uala.com.ar>
2024-02-26 19:58:11 -0300

    auth route

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -3,19 +3,22 @@ import { ML_BASE_URL, MLAuth } from "@/utils/meli";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  // const url = new URL(`${ML_BASE_URL}/oauth/token`);
-  // const res = await fetch(url, {
-  //   method: "POST",
-  //   headers: { ...MLAuth, "content-type": "application/x-www-form-urlencoded" },
-  //   body: JSON.stringify({
-  //     grant_type: "authorization_code",
-  //     client_id: "7523285884318227",
-  //     client_secret: "jlAMArxyPTlL4kQpSw9kxiNnF8a5T6JC",
-  //     redirect_uri: "https://search-car-xi.vercel.app/",
-  //   }),
-  // });
-  // const data = await res.json();
-  // console.log({ data });
-  // return NextResponse.json(data);
-  return NextResponse.json({ msg: "ok" });
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code}");
+  const url = new URL(`${ML_BASE_URL}/oauth/token`);
+  const res = await fetch(url, {
+    method: "POST",
+    headers: { ...MLAuth, "content-type": "application/x-www-form-urlencoded" },
+    body: JSON.stringify({
+      grant_type: "authorization_code",
+      client_id: "7523285884318227",
+      client_secret: "jlAMArxyPTlL4kQpSw9kxiNnF8a5T6JC",
+      redirect_uri: "https://search-car-xi.vercel.app/",
+      code: code,
+    }),
+  });
+  const data = await res.json();
+  console.log({ data });
+  return NextResponse.json(data);
+  // return NextResponse.json({ msg: "ok" });
 }
