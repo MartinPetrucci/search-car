d1bcff0e131141df7957d218939bda990340148d
martin-g-petrucci <martin.petrucci@uala.com.ar>
2024-03-26 23:19:18 -0300

    quitamos el fetch

--- src/app/favs/page.tsx
+++ src/app/favs/page.tsx
@@ -4,20 +4,20 @@ import { BASE_URL } from "@/utils/meli";
 
 export default async function FavCarsPage() {
   // const res = await fetch(`${BASE_URL}/api/fav-cars`);
-  const res = await fetch(`https://search-car-xi.vercel.app/api/fav-cars`);
-  const favCars = (await res.json()) as Car[];
-  console.log({ data: favCars });
+  // const res = await fetch(`https://search-car-xi.vercel.app/api/fav-cars`);
+  // const favCars = (await res.json()) as Car[];
+  // console.log({ data: favCars });
   return (
     <main>
       <h1>hola</h1>
-      {favCars && favCars.length > 0 && (
+      {/* {favCars && favCars.length > 0 && (
         <div>
           {favCars.map((car) => (
             <CarTooltip key={car.id} car={car} />
           ))}
         </div>
       )}
-      {JSON.stringify(favCars)}
+      {JSON.stringify(favCars)} */}
     </main>
   );
 }
