30c49b47f281ee7739abd64087b350644d273d66
martin-g-petrucci <martin.petrucci@uala.com.ar>
2024-03-26 23:13:41 -0300

    base_url

--- src/app/favs/page.tsx
+++ src/app/favs/page.tsx
@@ -3,7 +3,8 @@ import { Car } from "@/interfaces";
 import { BASE_URL } from "@/utils/meli";
 
 export default async function FavCarsPage() {
-  const res = await fetch(`${BASE_URL}/api/fav-cars`);
+  // const res = await fetch(`${BASE_URL}/api/fav-cars`);
+  const res = await fetch(`https://search-car-xi.vercel.app/api/fav-cars`);
   const favCars = (await res.json()) as Car[];
   console.log({ data: favCars });
   return (
