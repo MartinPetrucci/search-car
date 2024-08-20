6852d0821aca3703aaf5f656821ccff59373ebfc
martin-g-petrucci <martin.petrucci@uala.com.ar>
2024-08-20 00:05:39 -0300

    suspense boundary for useSearchParams

--- src/app/house/page.tsx
+++ src/app/house/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import SearchHouse from "@/components/SearchHouse/SearchHouse";
+import { Suspense } from "react";
 
 export default function HousePage() {
   return (
     <main className="flex min-h-screen flex-col p-12 gap-4">
-      <SearchHouse />
+      <Suspense>
+        <SearchHouse />
+      </Suspense>
     </main>
   );
 }
